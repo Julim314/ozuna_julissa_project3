09e63a20d95d13c3eccf7cbfa9879c4dc207d07a
Julim314 <72747996+Julim314@users.noreply.github.com>
2021-01-21 11:08:28 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+while True:
 
+    if input.sound_level() > 110:
+        light.set_all(color.rgb(100,25,50))
+        music.magic_wand.play_until_done()
+    else:
+        light.clear
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+while (true) {
+    if (input.soundLevel() > 110) {
+        light.setAll(color.rgb(100, 25, 50))
+        music.magicWand.playUntilDone()
+    } else {
+        light.clear
+    }
+    
+}
